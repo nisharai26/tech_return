58c7eba763a11a229738a847d11aa46ca8383076
nisharai26 <61697967+nisharai26@users.noreply.github.com>
2020-10-30 16:57:07 +0000

    Add files via upload

---
+++ problem_2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 11:04:43 2020
+
+@author: nisharai
+"""
+def past(h, m, s):
+    milisec=((h*3600)+(m*60)+s)*1000
+    print("Time converted in miliseconds:",milisec)
+    return 
+
+past(5,15,49)
